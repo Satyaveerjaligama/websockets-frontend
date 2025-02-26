792198e6cd7815ea4cf49ac3ff2969132561be92
Satyaveer Jaligama <satyaveerjaligama13@gmail.com>
2025-02-26 12:52:24 +0530

    Added redirection from home page to other pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,21 @@
-"use client";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-
-// initializing the connection
-const socket = io("http://localhost:8000");
+import Link from "next/link";
 
 export default function Home() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    socket.on("message", (message) => {
-      console.log("socket message", message);
-    });
-
-    return () => {
-      socket.off("message");
-      socket.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const response = await axios(
-          "http://localhost:8000/api/payments/get-data"
-        );
-        if (response.status === 200) {
-          setData(response.data.message);
-        }
-      } catch (error) {
-        console.log("error", error);
-      }
-    }
-    getData();
-  }, []);
   return (
     <>
-      <p className="text-xl font-bold">Web sockets</p>
-      {data}
+      <p>
+        <Link
+          href="/without-websocket"
+          className="hover:underline text-blue-600"
+        >
+          Without WebSockets
+        </Link>
+      </p>
+      <p>
+        <Link href="/with-websocket" className="hover:underline text-blue-600">
+          With WebSockets
+        </Link>
+      </p>
     </>
   );
 }
